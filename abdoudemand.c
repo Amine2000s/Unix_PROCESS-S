a41dd28e3bd055eedb0552f604d23e31629176d7
Amine2000s <92222814+Amine2000s@users.noreply.github.com>
2023-11-08 15:41:59 +0100

    Update abdoudemand.c
    made it actually work al hamdullillah

--- abdoudemand.c
+++ abdoudemand.c
@@ -4,9 +4,9 @@
 
 
 /*
-    code by chabi amine sif eddine 
-    this one demosnstrae a tree of a father  process and tow sons who have 2 sons each like this : 
-        
+    code by chabi amine sif eddine
+    this one demosnstrae a tree of a father  process and tow sons who have 2 sons each like this :
+
         []
         ||
     []--||--[]
@@ -20,44 +20,59 @@
 
 int main() {
 	//code
-	
-	
-	int x ,y ; 
+
+
+	int x ,y ;
 	x=fork();
 	y=fork();
-	
+    /*
+    *x=0 means that we are in the first child process
+    *
+    */
 	if(x==0 && y!=0){
-	    printf("i am the child no %d , my father pid is %d \n",getpid(),getppid());
-	    
+	    printf("i am the child no %d , my father pid is %d \n",getpid(),getppid(),x);
+        fflush(stdout);
 	    int z =fork();
-	    
-	    if(z==0){
+        wait(NULL);
+	    if(z==0 && x==0 && y!=0){
 	        	    printf("i am the child no %d , my father pid is %d \n",getpid(),getppid());
-                    wait(1);
-                    int g = fork();
-                    
-                    if(g==0)printf("i am the child no %d , my father pid is %d \n",getpid(),getppid());
 
-	    }else{
-	        
+	        	    int f= fork();
+	        	    wait(NULL);
+	        	    if(f==0 &&z==0 && x==0 && y!=0){
+                        	        	    printf("i am the child no %d , my father pid is %d \n",getpid(),getppid());
+
+	        	    }
 	    }
-	    
+
 	}
 
     if(y==0 && x!=0){
-        printf("iam the child no %d , my father pud is %d \n",getpid(),getppid());
-        
-        
-        int z = fork();
-        
+        printf("i am the child no %d , my father pid is %d \n",getpid(),getppid(),y);
+        fflush(stdout);
+
+       // int z = fork();
+       int z =fork();
+        wait(NULL);
+	    if(z==0 && x!=0 && y==0){
+	        	    printf("i am the child no %d , my father pid is %d \n",getpid(),getppid());
+                    wait(NULL);
+	        	    int f= fork();
+	        	    if(f==0 &&z==0 && x!=0 && y==0)
+                    {
+                        	        	    printf("i am the child no %d , my father pid is %d \n",getpid(),getppid());
+
+                    }
+	    }
+
     }
-    
+
     if(x!=0 && y!=0){
-        
+        wait(NULL);
         printf("i am actually the father and my sons x and y are : %d and %d \n",x,y);
-        
+
     }
-	
+
 //	printf(" x is %d / y is %d \n ",x,d)
 	return 0;
 }
